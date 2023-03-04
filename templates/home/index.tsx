e69401e65072a78e08b72f1ba2016a0fbb3d5f5a
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-03-03 21:43:29 -0300

    Santa Ceia

--- templates/home/index.tsx
+++ templates/home/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import STATE from "./state";
 
 export default function Index() {
   return (
@@ -6,76 +7,14 @@ export default function Index() {
       <h1>
         <img src="/assets/logo.svg" alt="Geração Ar" />
       </h1>
+
       <Items>
-        <Item
-          target="_blank"
-          href="https://www.youtube.com/watch?v=PosvOTlZJUA&list=PLhMbqdk2mNpf6ixlzBeYXxOsVTzTCpvVy&index=2"
-        >
-          Conhecimentos Esotéricos
-          <small>
-            Uma Introdução aos Conhecimentos Esotérico através de aulas gravadas
-            para turma de pós-graduação do Anicha em Blumenau no ano de 2020
-          </small>
-        </Item>
-        <Item
-          target="_blank"
-          href="https://www.youtube.com/watch?v=mgkl57C4VZc"
-        >
-          Jogo da Vida
-          <small>Palestra para ABPp em 2016</small>
-        </Item>
-        <Item
-          target="_blank"
-          href="https://www.youtube.com/watch?v=U5IP_1ADuSY"
-        >
-          Entrevista Numerologia
-          <small>
-            Entrevista para o programa Toda Manhã do RBA TV de Rio do Sul em
-            2018
-          </small>
-        </Item>
-        <Item target="_blank" href="/geracoes-e-os-quatro-elementos">
-          Gerações
-          <small>
-            Como as Gerações obedecem ao ritmo da Nova Ordem dos 4 Elementos
-          </small>
-        </Item>
-        <Item target="_blank" href="http://setenios.geracaoar.com.br/">
-          Setênios
-          <small>Aplicativo online</small>
-        </Item>
-        <Item target="_blank" href="http://numerologo.epanel.com.br/">
-          Numerólogo
-          <small>Sistema online para cálculo numerológico</small>
-        </Item>
-        <Item target="_blank" href="/numerologia">
-          Numerologia e a Jornada Interior
-          <small>Síntese das fases da vida numa visão numerológica</small>
-        </Item>
-        <Item target="_blank" href="/astrologia-e-auto-lideranca">
-          Astrologia e a Auto-liderança
-          <small>
-            O nascimento do Líder e suas fases conforme o ritmo do zodíaco
-          </small>
-        </Item>
-        <Item target="_blank" href="/ciclos-da-historia">
-          Ciclos da História
-          <small>
-            Uma síntese alquímica dos últimos 600 anos e uma previsão para os
-            próximos 200.
-          </small>
-        </Item>
-        <Item target="_blank" href="http://metanoia.geracaoar.com.br/">
-          Metanoia
-          <small>Aplicativo online</small>
-        </Item>
-        <Item target="_blank" href="http://prosperacao.com.br/">
-          Prosperação
-          <small>
-            Os 4 Pilares e as 8 Áreas para construção de Comunidades de
-            Aprendizagem para a Era da Sabedoria
-          </small>
-        </Item>
+        {STATE.items.map((item, i) => (
+          <Item key={i} target="_blank" href={item.href}>
+            {item.title}
+            <small>{item.description}</small>
+          </Item>
+        ))}
       </Items>
     </Wrapper>
   );
---
+++ templates/home/state.tsx
@@ -0,0 +1,74 @@
+const STATE = {
+  items: [
+    {
+      title: "Jogo da Vida",
+      href: "https://www.youtube.com/watch?v=mgkl57C4VZc",
+      description:
+        "Palestra para ABPp em 2016 usando arquétipos dos 4 Elementos para explicar a jornada da Vida Acadêmica e Profissional",
+    },
+    {
+      title: "Conhecimentos Esotéricos",
+      href: "https://www.youtube.com/watch?v=PosvOTlZJUA&list=PLhMbqdk2mNpf6ixlzBeYXxOsVTzTCpvVy&index=2",
+      description:
+        "Uma Introdução aos Conhecimentos Esotérico através de aulas gravadas para turma de pós-graduação do Anicha em Blumenau no ano de 2020",
+    },
+    {
+      title: "Entrevista Numerologia",
+      href: "https://www.youtube.com/watch?v=U5IP_1ADuSY",
+      description:
+        "Entrevista para o programa Toda Manhã do RBA TV de Rio do Sul em 2018",
+    },
+    {
+      title: "Gerações",
+      href: "/geracoes-e-os-quatro-elementos",
+      description:
+        "Como as Gerações obedecem ao ritmo da Nova Ordem dos 4 Elementos",
+    },
+    {
+      title: "Setênios",
+      href: "https://setenios.geracaoar.com.br/",
+      description: "Aplicativo online",
+    },
+    {
+      title: "Numerólogo",
+      href: "http://numerologo.epanel.com.br/",
+      description: "Sistema online para cálculo numerológico",
+    },
+    {
+      title: "Numerologia e a Jornada Interior",
+      href: "/numerologia",
+      description: "Síntese das fases da vida numa visão numerológica",
+    },
+    {
+      title: "Astrologia e a Auto-liderança",
+      href: "/astrologia-e-auto-lideranca",
+      description:
+        "O nascimento do Líder e suas fases conforme o ritmo do zodíaco",
+    },
+    {
+      title: "Ciclos da História",
+      href: "/ciclos-da-historia",
+      description:
+        "Uma síntese alquímica dos últimos 600 anos e uma previsão para os próximos 200.",
+    },
+    {
+      title: "Metanoia",
+      href: "http://metanoia.geracaoar.com.br",
+      description: "Aplicativo online",
+    },
+    {
+      title: "Prosperação",
+      href: "https://prosperacao.com.br/",
+      description:
+        "Os 4 Pilares e as 8 Áreas para construção de Comunidades de Aprendizagem para a Era da Sabedoria",
+    },
+    {
+      title: "Santa Ceia",
+      href: "http://santaceia.geracaoar.com.br/",
+      description:
+        "Uma visão Astrológica, Astronômica e Neurocientífica numa das obras mais famosas de Leonardo da Vinci",
+    },
+  ],
+};
+
+export default STATE;
